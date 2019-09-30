96667ea15c867631b50d241927ed86ed37d16949
AustinPotts <pottsevolvements@gmail.com>
2019-09-30 15:48:12 -0400

    Configured Part 1(Layout)

---
+++ Recipes/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Austin Potts on 9/30/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func searchField(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
